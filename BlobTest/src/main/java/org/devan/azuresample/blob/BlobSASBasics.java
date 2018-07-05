ccac8e82993d712daf890543458cb4fc54a98dc6
DEVAN M S <msdevanms@gmail.com>
2018-07-05 11:44:05 +0530

    Added Headers, HTTS and IP Range support

--- BlobTest/src/main/java/org/devan/azuresample/blob/BlobSASBasics.java
+++ BlobTest/src/main/java/org/devan/azuresample/blob/BlobSASBasics.java
@@ -10,18 +10,20 @@ import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.IPRange;
+import com.microsoft.azure.storage.SharedAccessProtocols;
 import com.microsoft.azure.storage.blob.BlobContainerPermissions;
 import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
 import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.SharedAccessBlobHeaders;
 import com.microsoft.azure.storage.blob.SharedAccessBlobPermissions;
 import com.microsoft.azure.storage.blob.SharedAccessBlobPolicy;
 
 /**
  * 
- * @author devan
- * Expecting cloud storage connection string as argument
+ * @author devan Expecting cloud storage connection string as argument
  */
 public class BlobSASBasics {
 
@@ -40,7 +42,6 @@ public class BlobSASBasics {
 			// Create the container if it does not exist
 			container.createIfNotExists();
 
-			// Make the container public
 			// Create a permissions object
 			BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
 
@@ -50,7 +51,8 @@ public class BlobSASBasics {
 			// Set the permissions on the container
 			container.uploadPermissions(containerPermissions);
 
-			// Set the permission for SAS and expire time as 1 hour from now (3600 milli seconds)
+			// Set the permission for SAS and expire time as 1 hour from now (3600 milli
+			// seconds)
 			SharedAccessBlobPolicy sp = createSharedAccessPolicy(
 					EnumSet.of(SharedAccessBlobPermissions.READ, SharedAccessBlobPermissions.LIST), 3600);
 
@@ -59,12 +61,28 @@ public class BlobSASBasics {
 
 			// Upload text to the blob
 			blob.uploadText("Hello, SAS");
+
+			// IP range can access blob SAS url - Change as your requirement
+			IPRange ipRange = new IPRange("0.0.0.0", "255.255.255.255");
+
+			// Sample Headers
+			SharedAccessBlobHeaders headers = new SharedAccessBlobHeaders();
+			headers.setCacheControl("no-cache");
+			headers.setContentDisposition("attachment; filename=\"fname.ext\"");
+			headers.setContentEncoding("gzip");
+			headers.setContentLanguage("da");
+			headers.setContentType("text/html; charset=utf-8");
+
 			// sas token
-			String sasToken = blob.generateSharedAccessSignature(sp, null);
-			System.out.println(sasToken);
+			String sasToken = blob.generateSharedAccessSignature(sp, headers, null, ipRange,
+					SharedAccessProtocols.HTTPS_ONLY);
+
+			// String sasToken = blob.generateSharedAccessSignature(sp, null);
+
+			System.out.println(MessageFormat.format("SAS Token : {0}", sasToken));
 
 			String blobURLWithSas = MessageFormat.format("{0}?{1}", blob.getUri().toURL(), sasToken);
-			System.out.println(blobURLWithSas);
+			System.out.println(MessageFormat.format("Blob url with SAS : {0}", blobURLWithSas));
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
